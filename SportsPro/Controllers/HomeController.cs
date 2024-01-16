b756a59c5e93d170c7019ee20d914feb6dc2f23f
Kylan Larson <kylan.larson@southeasttech.edu>
2024-01-16 17:52:28 -0600

    Got it so you can view the product management site

--- SportsPro/Controllers/HomeController.cs
+++ SportsPro/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using SportsPro.Models;
 using System.Linq;
 
--- SportsPro/Controllers/ProductController.cs
+++ SportsPro/Controllers/ProductController.cs
@@ -1,12 +1,70 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SportsPro.Models;
+using System.Linq;
 
 namespace SportsPro.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Index()
+        private SportsProContext context { get; set; }
+
+        public ProductController(SportsProContext ctx)
         {
-            return View();
+            context = ctx;
         }
+
+        [HttpGet]
+        public IActionResult Add() 
+        {
+            ViewBag.Action = "Add";
+            return View("Edit", new Product());
+        }
+        [HttpGet]
+        public IActionResult Edit(int id) 
+        {
+            ViewBag.Action = "Edit";
+            var product = context.Products.Find(id);
+            return View(product);
+        }
+        [HttpPost]
+        public IActionResult Edit(Product product) 
+        {
+            if (ModelState.IsValid)
+            {
+               if (product.ProductID == 0) 
+               
+                    context.Products.Add(product);
+               else
+                    context.Products.Update(product);
+                     context.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                ViewBag.Action = (product.ProductID == 0) ? "Add": "Edit";
+                return View(product);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var product = context.Products.Find(id);
+            return View(product);
+        }
+        [HttpPost]
+        public IActionResult Delete(Product product)
+        {
+            context.Products.Remove(product);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult List() 
+        { 
+            var products = context.Products.ToList();
+            return View(products); 
+        }  
     }
 }
