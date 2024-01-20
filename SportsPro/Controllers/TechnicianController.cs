db64b514ad27b813a3081993849b86432cfe5961
Connor Arends <connor.arends@southeasttech.edu>
2024-01-20 17:36:16 -0600

    Added technician list and info to the controller and model

--- SportsPro/Controllers/TechnicianController.cs
+++ SportsPro/Controllers/TechnicianController.cs
@@ -1,12 +1,74 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SportsPro.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SportsPro.Controllers
 {
     public class TechnicianController : Controller
     {
-        public IActionResult Index()
+        private SportsProContext context { get; set; }
+
+        public TechnicianController(SportsProContext ctx)
         {
-            return View();
+            context = ctx;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Add";
+            return View("Edit", new Technician());
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Action = "Edit";
+            var technician = context.Technicians.Find(id);
+            return View("Edit", technician);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Technician technician)
+        {
+            if (ModelState.IsValid)
+            {
+                if (technician.TechnicianID == 0)
+                    context.Technicians.Add(technician);
+                else
+                    context.Technicians.Update(technician);
+
+                context.SaveChanges();
+                return RedirectToAction("List");
+            }
+            else
+            {
+                ViewBag.Action = (technician.TechnicianID == 0) ? "Add" : "Edit";
+                return View("Edit", technician);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var technician = context.Technicians.Find(id);
+            return View(technician);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Technician technician)
+        {
+            context.Technicians.Remove(technician);
+            context.SaveChanges();
+            return RedirectToAction("List");
+        }
+
+        public IActionResult List()
+        {
+            var technicians = context.Technicians.ToList();
+            return View(technicians);
         }
     }
+
 }
--- SportsPro/Models/Technician.cs
+++ SportsPro/Models/Technician.cs
@@ -1,19 +1,21 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace SportsPro.Models
 {
     public class Technician
     {
-		public int TechnicianID { get; set; }	   
+        public int TechnicianID { get; set; }
 
-		[Required]
-		public string Name { get; set; }
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; }
 
-		[Required]
-		public string Email { get; set; }
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string Email { get; set; }
 
-		[Required]
-		public string Phone { get; set; }
-	}
+        public string Phone { get; set; }
+
+    }
 }
