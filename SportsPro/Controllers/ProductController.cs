78f193be10d1cd053cc7c51a7c068dce7cda1409
Kylan Larson <kylan.larson@southeasttech.edu>
2024-01-26 20:08:56 -0600

    just a small change

--- SportsPro/Controllers/ProductController.cs
+++ SportsPro/Controllers/ProductController.cs
@@ -66,7 +66,7 @@ namespace SportsPro.Controllers
             TempData["delete"] = product.Name + " has been deleted!";
             context.Products.Remove(product);
             context.SaveChanges();
-            return RedirectToAction("Product", "Product");
+            return RedirectToAction("Product");
         }
 
         public ViewResult Product() 
